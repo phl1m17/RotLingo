4fbed7e333e74a28eb5491e287c8b6ada392e02c
phl1m17 <clarkmanag@gmail.com>
2024-12-02 15:15:20 -0500

    thing is still broken but added to different question class

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,59 +1,29 @@
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 
 public final class GameScreen implements ActionListener{
-    Panel panel;
-    JButton[]buttons = new JButton[4];
-    JLabel question;
-    ArrayList<String>questions;
     int questionCount = 0;
+    //Question Classes
+    Question1 q1;
+
+    ArrayList<String>question1Type;
     public GameScreen(Panel panel) {
-        this.panel = panel;
-
-        question = new JLabel();
-        question.setBounds(20, 20, 360, 50);
-        question.setFont(panel.getFont().deriveFont(20f));
-        question.setForeground(Color.white);
-
-        questions = new ArrayList<>(Arrays.asList("question 1","question 2","question 3","question 4"));
-        questions(questionCount);
-        
-        for(int i = 0; i<buttons.length;i++){
-            buttons[i] = new JButton(String.valueOf((i+1)));
-            buttons[i].setBounds((i<2?((i+1)):(i<4?(i-1):(i-3)))*195-172, (i<2?70:(i<4?150:390))+400, 165, 60);
-            buttons[i].setBackground(new Color(111, 228, 21));
-            buttons[i].setForeground(Color.white);
-            buttons[i].setFont(panel.getFont().deriveFont(30f));
-            buttons[i].setOpaque(true);
-            buttons[i].setBorder(BorderFactory.createLineBorder(panel.getDuoGreen(), 4));
-            buttons[i].setForeground(Color.white);
-            buttons[i].addActionListener(this);
-        }
+        question1Type = new ArrayList<>(Arrays.asList("Question 1", "Question2"));
+        q1 = new Question1(this, panel);
     }
     public void addComponents(){
-        for(JButton b : buttons){
-            panel.add(b);
-        }
-        panel.add(question);
-    }
-    public void questions(int level){
-        question.setText(questions.get(level));
+        q1.setQuestion(question1Type.get(questionCount));
+        q1.addComponents();
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==buttons[0]){
+        if(e.getSource() == q1.buttons[0]){
+            System.out.println("a");
             questionCount++;
-            questions(questionCount);
-        }
-        else{
-            question.setText("Incorrect");
+            q1.setQuestion(question1Type.get(questionCount));
         }
     }
 }
---
+++ src/Question1.java
@@ -0,0 +1,51 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class Question1 implements  ActionListener{
+    JButton[] buttons = new JButton[4];
+    Panel panel;
+    GameScreen gameScreen;
+    JLabel question;
+
+    //Multiple Choice Type Question
+    public Question1(GameScreen gameScreen, Panel panel) {
+        this.panel = panel;
+        this.gameScreen = gameScreen;
+
+        for(int i = 0; i<buttons.length;i++){
+            buttons[i] = new JButton(String.valueOf((i+1)));
+            buttons[i].setBounds((i<2?((i+1)):(i<4?(i-1):(i-3)))*195-172, (i<2?70:(i<4?150:390))+400, 165, 60);
+            buttons[i].setBackground(new Color(111, 228, 21));
+            buttons[i].setForeground(Color.white);
+            buttons[i].setFont(panel.getFont().deriveFont(30f));
+            buttons[i].setOpaque(true);
+            buttons[i].setBorder(BorderFactory.createLineBorder(panel.getDuoGreen(), 4));
+            buttons[i].setForeground(Color.white);
+            buttons[i].addActionListener(this);
+        }
+
+        question = new JLabel();
+        question.setBounds(20, 20, 360, 50);
+        question.setFont(panel.getFont().deriveFont(20f));
+        question.setForeground(Color.white);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == buttons[0]){
+            System.out.println("a");
+        }
+    }
+    public void addComponents(){
+        for(JButton b : buttons){
+            panel.add(b);
+        }
+        panel.add(question);
+    }
+    public void setQuestion(String question){
+        this.question.setText(question);
+    }
+}
