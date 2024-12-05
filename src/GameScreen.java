f4f75e75975a950ddad8ae8286f6155f50a09d3e
phl1m17 <clarkmanag@gmail.com>
2024-12-05 10:41:32 -0500

    e

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -11,25 +10,21 @@ public final class GameScreen{
     Question[] questions = new Question[8];
 
     public GameScreen(Panel panel) {
-        // String[] opts = {"Cool Points", "Older Male", "Not Lying", "Charisma"};
-        // try {
-        //     questions[questionCount] = new SoundMultipleChoiceQuestion(panel, this, opts, 3, "src\\Sounds\\Rizz.wav");
-        // } catch (UnsupportedAudioFileException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // } catch (IOException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // } catch (LineUnavailableException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // }
+        String[] opts = {"Cool Points", "Older Male", "Not Lying", "Charisma"};
+        try {
+            questions[questionCount] = new SoundMultipleChoiceQuestion(panel, this, opts, 3, "src/Sounds/Rizz.wav");
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {}
 
         // String[] opts = {"reality", "vibe level", "Tommy", "standard"};
-        // questions[questionCount] = new MultipleChoiceQuestion(this, panel, "Unc", opts, 2);
+        // questions[questionCount] = new MultipleChoiceQuestion(panel, this, "Unc", opts, 2);
+        // String[][] opts = {{"Aura", "Unc", "No Cap", "Rizz", "Sigma"},{"Cool Points", "Older Male", "Not Lying", "Charisma", "Cool Person"}};
+        // questions[questionCount] = new MatchingQuestion(panel, this, opts);
         
-        String[][] opts = {{"Aura", "Unc", "No Cap", "Rizz", "Sigma"},{"Cool Points", "Older Male", "Not Lying", "Charisma", "Cool Person"}};
-        questions[questionCount] = new MatchingQuestion(panel, this, opts);
+        // String[] opts = {"reality", "vibe level", "Tommy", "standard"};
+        // questions[questionCount] = new MultipleChoiceQuestion(panel, this, "Unc", opts, 2);
+        
+        // String[][] opts = {{"Aura", "Unc", "No Cap", "Rizz", "Sigma"},{"Cool Points", "Older Male", "Not Lying", "Charisma", "Cool Person"}};
+        // questions[questionCount] = new MatchingQuestion(panel, this, opts);
     }
     public void addComponents(){
         questions[questionCount].setQuestion("Question " + (questionCount+1));
